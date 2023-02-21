fbd63ab90e7250d02242dc4531f329343cb0409c
Radek Rocławski <r.roclawski@ankara.pl>
2023-02-21 14:09:02 +0100

    Added project description in main file of the game

--- poker.py
+++ poker.py
@@ -1,3 +1,7 @@
+# Classic poker game. Human vs Human or Human vs AI. No bids just shuffling cards, 
+# exchange turn and finally - result. Showing not only the winner but also his "hand" with description
+# this is the main game file.
+
 a = "A \u2664"
 b = "K \u2661"
 c = "D \u2662"
